bab66f320c758c0f42914051994deefbc14b6e7e
laksiduoficial <maheshnimsralaksidu123@gmail.com>
2024-10-16 10:24:40 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -15204,8 +15204,8 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "©Ibrahim Adams",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "254710772666",
+  OWNER_NAME: process.env.OWNER_NAME || "©laksidu",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "94771470396",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
